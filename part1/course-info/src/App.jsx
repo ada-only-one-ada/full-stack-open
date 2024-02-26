e5a61ff940ceb13537100200c2235f718f3d045a
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-02-25 22:38:14 -0800

    finished part1 - 1.2

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-  const course = '1.1 Half Stack application development'
+  const course = '1.2 Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -25,10 +25,16 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
   )
 }
 
