ce7855e9636ebefec9ab205011d33e6d938cb6f4
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-02-27 15:21:43 -0800

    finished part2 course-info 2.5 a separate Course component

--- part2/course-info/src/App.jsx
+++ part2/course-info/src/App.jsx
@@ -1,39 +1,4 @@
-const Header = ({ course }) => <h3>{course}</h3>
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>{name} {exercises}</p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part =>
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      )}
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  const array = parts.map(part => part.exercises);
-  const total = array.reduce((total, val) => total + val, 0);
-
-  return (
-    <h4>total of {total} exercises</h4>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const Courses = ({ courses }) => {
   return (
