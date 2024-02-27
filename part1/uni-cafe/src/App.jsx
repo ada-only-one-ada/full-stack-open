402296487f68a7969a77473b71d0ed884d254c39
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-02-27 10:22:55 -0800

    finished uni-cafe 1.10 create statisticLine component

--- part1/uni-cafe/src/App.jsx
+++ part1/uni-cafe/src/App.jsx
@@ -20,11 +20,10 @@ const App = () => {
 
   return (
     <div>
-      <Header text="1.9 give feedback" />
+      <Header text="1.10 give feedback" />
       <Button handleClick={handleGoodClick} text="good" />
       <Button handleClick={handleNeutralClick} text="neutral" />
       <Button handleClick={handleBadClick} text="bad" />
-
       <Header text="statistics" />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
@@ -37,23 +36,29 @@ const Button = ({ handleClick, text }) => {
   )
 }
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const obj = {
     total: total,
-    average: total > 0 ? (good * 1 + neutral * 0 + bad * (-1)) / total : 0,
-    positive: total > 0 ? (good / total * 100) : 0,
+    average: (good * 1 + neutral * 0 + bad * (-1)) / total,
+    positive: good / total * 100 + " %",
   }
 
   if (total > 0) {
     return (
       <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {obj.total}</p>
-        <p>average {obj.average}</p>
-        <p>positive {obj.positive} %</p>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={obj.total} />
+        <StatisticLine text="average" value={obj.average} />
+        <StatisticLine text="positive" value={obj.positive} />
       </div>
     )
   }
