ba3119f6c53ba4444c24339671bf972dcad2bcb4
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-02-27 16:40:29 -0800

    finished part2 phone-book 2.8 add new phone number

--- part2/phone-book/src/App.jsx
+++ part2/phone-book/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', id: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -13,17 +14,22 @@ const App = () => {
     } else {
       const personObject = {
         name: newName,
+        number: newNumber,
         id: newName,
       }
 
       setPersons(persons.concat(personObject));
       setNewName('');
-
+      setNewNumber('');
     }
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   }
 
   return (
@@ -32,13 +38,14 @@ const App = () => {
 
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div><button type="submit">add</button></div>
       </form>
 
       <h2>Numbers</h2>
 
       {persons.map(person =>
-        <div key={person.id}> {person.name}</div>
+        <div key={person.id}> {person.name} {person.number}</div>
       )}
     </div >
   )
