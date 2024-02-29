004a38c687488671071f27d9725962b74a441395
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-02-29 15:31:07 -0800

    finished part2 phone-book 2.11 fetch data from db.json

--- part2/note/src/App.jsx
+++ part2/note/src/App.jsx
@@ -1,24 +1,60 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  // uses the useState function to initialize the piece of state stored in notes with an empty array
-  const [notes, setNotes] = useState(props.notes);
-
-  const [newNote, setNewNote] = useState("a new note...");
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  /* 
+  The following function/effect is executed immediately after rendering. 
+  The execution of the function results in effect being printed to the console, 
+  and the command axios.get initiates the fetching of data from the server as well as registers the 'response' function as an event handler for the operation.
+  */
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled');
+        setNotes(response.data);
+      })
+  }, []);
+  // The function useEffect takes two parameters: 
+  // The first is a function, the effect itselft: By default, effects run after every completed render, but you can choose to fire it only when certain values have changed.
+  // So by default, the effect is always run after the component has been rendered. 
+  // The second is used to specify how often the effect is run. If it is an empty array[], then the effect is only run along with the first render of the component.
+
+  /*
+  // A reference to an event handler function is assigned to the variable eventHandler. 
+  // The promise returned by the get method of Axios is stored in the variable promise. 
+  // The registration of the callback happens by giving the eventHandler variable, 
+  // referring to the event-handler function, as a parameter to the then method of the promise. 
+  useEffect(() => {
+    console.log("effect");
+
+    const eventHandlerRef = response => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    }
+    const promise = axios.get("http://localhost:3001/notes");
+    promise.then(eventHandlerRef);
+  }, []);
+  */
+
+  // The body of the function defining the component is executed and the component is rendered for the first time.
+  // At this point render 0 notes is printed, meaning datat hasn't been fetched from the server yet.
+  console.log('render', notes.length, 'notes');
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important);
 
-  // The event parameter is the event that triggers the call to the event handler function:
-  // The event handler immediately calls the event.preventDefault() method, which prevents the default action of submitting a form. 
-  // The default action would, among other things, cause the page to reload.
   const addNote = (event) => {
     event.preventDefault();
 
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5, //our note has a 50% chance of being marked as important.
+      important: Math.random() < 0.5,
       id: notes.length + 1,
     }
     console.log(noteObject.important);
@@ -26,14 +62,10 @@ const App = (props) => {
     setNewNote('');
   }
 
-  // The event handler is called every time a change occurs in the input element.
-  // The event handler function receives the event object as its event parameter:
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value);
   }
-  // The target property of the event object now corresponds to the controlled input element, 
-  // and event.target.value refers to the input value of that element.
 
   return (
     <div>
--- part2/phone-book/src/App.jsx
+++ part2/phone-book/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newSearchWord, setNewSearchWord] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [newSearchWord, setNewSearchWord] = useState('');
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        setPersons(response.data);
+      })
+  }, []);
+  console.log('render', persons.length, 'persons');
 
   const addPerson = (event) => {
     event.preventDefault();
