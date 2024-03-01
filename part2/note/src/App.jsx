1c65c7fe0962d42a9ca30d215c3ecf0c14126dec
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-02-29 22:44:34 -0800

    add button to change the importance of note and so it also updates the json file

--- part2/note/src/App.jsx
+++ part2/note/src/App.jsx
@@ -7,11 +7,6 @@ const App = () => {
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
-  /* 
-  The following function/effect is executed immediately after rendering. 
-  The execution of the function results in effect being printed to the console, 
-  and the command axios.get initiates the fetching of data from the server as well as registers the 'response' function as an event handler for the operation.
-  */
   useEffect(() => {
     console.log('effect');
     axios
@@ -21,45 +16,42 @@ const App = () => {
         setNotes(response.data);
       })
   }, []);
-  // The function useEffect takes two parameters: 
-  // The first is a function, the effect itselft: By default, effects run after every completed render, but you can choose to fire it only when certain values have changed.
-  // So by default, the effect is always run after the component has been rendered. 
-  // The second is used to specify how often the effect is run. If it is an empty array[], then the effect is only run along with the first render of the component.
-
-  /*
-  // A reference to an event handler function is assigned to the variable eventHandler. 
-  // The promise returned by the get method of Axios is stored in the variable promise. 
-  // The registration of the callback happens by giving the eventHandler variable, 
-  // referring to the event-handler function, as a parameter to the then method of the promise. 
-  useEffect(() => {
-    console.log("effect");
-
-    const eventHandlerRef = response => {
-      console.log("promise fulfilled");
-      setNotes(response.data);
-    }
-    const promise = axios.get("http://localhost:3001/notes");
-    promise.then(eventHandlerRef);
-  }, []);
-  */
-
-  // The body of the function defining the component is executed and the component is rendered for the first time.
-  // At this point render 0 notes is printed, meaning datat hasn't been fetched from the server yet.
-  console.log('render', notes.length, 'notes');
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important);
 
+  // We create a new object for the note but omit the id property since it's better to let the server generate ids for our resources.
   const addNote = (event) => {
     event.preventDefault();
-
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     }
-    console.log(noteObject.important);
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+    // The object is sent to the server using the axios post method.
+    // The registered event handler logs the response that is sent back from the server to the console.
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data));
+        setNewNote("");
+        console.log(response)
+      })
+  }
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`; // This defines the unique URL for each note resource based on its id.
+    const note = notes.find(n => n.id === id); // The array find method is used to find the note we want to modify, and we then assign it to the note variable.
+    const changedNote = { ...note, important: !note.important }; // After this, we create (shallow copy) a new object that is an exact copy of the old note, apart from the important property that has the value flipped (from true to false or from false to true).
+
+    //The new note is then sent with a PUT request to the backend where it will replace the old object.
+    // The callback function sets the component's notes state to a new array that contains all the items from the previous notes array, 
+    // except for the old note which is replaced by the updated version of it returned by the server:
+    axios.put(url, changedNote)
+      .then(response => {
+        // The map method creates a new array by mapping every item from the old array into an item in the new array.
+        // If the id is not the changed one, we simply copy the item from the old array into the new array.
+        // If the id is the changed one, the note object returned by the server is addred to the array 
+        setNotes(notes.map(n => n.id !== id ? n : response.data));
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -75,7 +67,7 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         )}
       </ul>
 
