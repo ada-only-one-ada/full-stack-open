a1f42520aff7217566b7ee18ba84369d54b189da
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-05-01 21:08:37 -0700

    change baseUrl to fly.io

--- part2/note/src/services/notes.js
+++ part2/note/src/services/notes.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const baseUrl = '/api/notes';
+//const baseUrl = '/api/notes';
+const baseUrl = "https://full-stack-note-app.fly.dev/api/notes"
 
 /* 
 instead of the entire HTTP response, we would only get the response data. 
